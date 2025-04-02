ad7462f02deba57b02051c69750229e4aeb39c66
tanishqtiwari7 <tanishqtiwari2020@gmail.com>
2025-04-02 13:30:34 +0530

    Enhance headless server support by allowing command-line argument for headless mode

--- src/main/java/dev/uday/Main.java
+++ src/main/java/dev/uday/Main.java
@@ -5,6 +5,7 @@ import dev.uday.GUI.ServerApp;
 import dev.uday.Headless.HeadlessServer;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,6 +21,10 @@ public class Main {
 
         if (headless || "true".equalsIgnoreCase(headlessProperty)) {
             HeadlessServer.startHeadlessServer(args);
+        } else if (Objects.equals(args[0], "headless")) {
+            String[] args2 = new String[args.length - 1];
+            System.arraycopy(args, 1, args2, 0,args.length - 1);
+            HeadlessServer.startHeadlessServer(args2);
         } else {
             // Launch the UI (will show splash screen then config dialog)
             ServerApp.startServerApp();
